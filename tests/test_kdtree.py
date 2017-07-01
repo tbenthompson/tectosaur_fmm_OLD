8ec7930f8439aea3f45ef78a464dc221b8d5df86
Ben Thompson <t.ben.thompson@gmail.com>
2017-06-30 22:45:15 -0400

    Forgot files.

---
+++ tests/test_kdtree.py
@@ -0,0 +1,50 @@
+import numpy as np
+import tectosaur_fmm.fmm_wrapper as fmm
+
+def test_kdtree_bisects():
+    pts = np.random.rand(100,3)
+    kdtree = fmm.KDTree(pts, pts, 1)
+    pts = np.array(kdtree.pts)
+    for n in kdtree.nodes:
+        if n.is_leaf:
+            continue
+        l = kdtree.nodes[n.children[0]]
+        r = kdtree.nodes[n.children[1]]
+        assert(l.start == n.start)
+        assert(r.end == n.end)
+        assert(l.end == r.start)
+
+def test_kdtree_contains_pts():
+    pts = np.random.rand(100,3)
+    kdtree = fmm.KDTree(pts, pts, 1)
+    pts = np.array(kdtree.pts)
+    for n in kdtree.nodes:
+        for d in range(3):
+            dist = np.sqrt(
+                np.sum((pts[n.start:n.end,:] - n.bounds.center) ** 2, axis = 1)
+            )
+            assert(np.all(dist < n.bounds.r * 1.0001))
+
+def test_kdtree_height_depth():
+    pts = np.random.rand(100,3)
+    kdtree = fmm.KDTree(pts, pts, 1)
+    for n in kdtree.nodes:
+        if n.is_leaf:
+            continue
+        for c in range(2):
+            assert(n.depth == kdtree.nodes[n.children[c]].depth - 1)
+        assert(n.height ==
+            max([kdtree.nodes[n.children[c]].height for c in range(2)]) + 1)
+
+def test_kdtree_orig_idx():
+    pts = np.random.rand(100,3)
+    kdtree = fmm.KDTree(pts, pts, 1)
+    for i, orig_i in enumerate(kdtree.orig_idxs):
+        np.testing.assert_almost_equal(kdtree.pts[i], pts[orig_i, :], 10)
+
+def test_kdtree_idx():
+    pts = np.random.rand(100,3)
+    kdtree = fmm.KDTree(pts, pts, 1)
+    for i, n in enumerate(kdtree.nodes):
+        assert(n.idx == i)
+
---
+++ tests/test_misc.cpp
@@ -0,0 +1,21 @@
+#include "fmm_impl.hpp"
+#include "doctest.h"
+#include "test_helpers.hpp"
+
+TEST_CASE("inscribe") {
+    auto s =
+        inscribe_surf({{1, 1, 1}, 2}, 0.5, {{1, 0, 0}, {0, 1, 0}, {0, 0, 1}});
+    REQUIRE(s.size() == size_t(3));
+    REQUIRE_ARRAY_EQUAL(s[0], Vec3{2, 1, 1}, 3);
+    REQUIRE_ARRAY_EQUAL(s[1], Vec3{1, 2, 1}, 3);
+    REQUIRE_ARRAY_EQUAL(s[2], Vec3{1, 1, 2}, 3);
+}
+
+TEST_CASE("matvec") {
+    BlockSparseMat m{{{1, 1, 2, 2, 0}}, {0, 2, 1, 3}};
+    std::vector<double> in = {0, -1, 1};
+    auto out = m.matvec(in.data(), 3);
+    REQUIRE(out[0] == 0.0);
+    REQUIRE(out[1] == 2.0);
+    REQUIRE(out[2] == 2.0);
+}
